7c6fce65380e11887a429359d08052739d3ada1e
Abe <abe@humboldt.edu>
2019-08-18 12:35:59 -0700

    Fixed node-Gpy error and switched to net-ping

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
+var ping = require ("net-ping");
 
 
-console.log('ayyyy');
+var options = {
+    networkProtocol: ping.NetworkProtocol.IPv4,
+    packetSize: 16,
+    retries: 1,
+    sessionId: (process.pid % 65535),
+    timeout: 1000,
+    ttl: 128
+};
+
+var session = ping.createSession (options);
+
+session.pingHost ('8.8.8.8', function (error, target,sent, recv) {
+    var ms = recv - sent;
+    if (error)
+        console.log (target + ": " + error.toString ());
+    else
+        console.log (ms + 'ms');
+});
+
+session.pingHost ('192.168.1.5', function (error, target,sent, recv) {
+    var ms = recv - sent;
+    if (error)
+        console.log (target + ": " + error.toString ());
+    else
+        console.log (ms + 'ms');
+});
+
+session.pingHost ('8.8.8.27', function (error, target,sent, recv) {
+    var ms = recv - sent;
+    if (error)
+        console.log (target + ": " + error.toString ());
+    else
+        console.log (ms + 'ms');
+});

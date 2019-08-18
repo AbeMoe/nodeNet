fad90cd3cce605078c0cb76d40763c7768629788
Abe <abe@humboldt.edu>
2019-08-18 15:05:23 -0700

    Switched to "ping" library as it may actually be a better fit.

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
-var ping = require ("net-ping");
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+var ping = require('ping');
 
-IPs = ['216.58.194.46','40.112.72.205','188.125.72.165']
+
+var hosts = ['192.168.1.1', 'google.com', 'yahoo.com'];
+
+
+
+hosts.forEach(function (host) {
+    // WARNING: -i 2 argument may not work in other platform like window
+    ping.promise.probe(host, {
+        timeout: 1000,
+    }).then(function (res) {
+        console.log(res);
+    });
+});
+/*
+
+
 
 var options = {
     networkProtocol: ping.NetworkProtocol.IPv4,
     packetSize: 16,
     retries: 1,
-    timeout: 1500,
+    sessionId: (process.pid % 65535),
+    timeout: 5000,
     ttl: 128
 };
 
-var session = ping.createSession (options);
+
+
+
+const IPs = ['1.1.1.1','40.112.72.205','185.11.125.117','216.58.194.46', '1.2.3.4']
+
+
+
 
 
 async function pingCheck(target)
 {
+  let session = ping.createSession (options);
   let promise = new Promise((resolve,reject)=>
   {
     session.pingHost (target,
-    function (error, target,sent, recv)
+    async function (error, target,sent, recv)
     {
         var ms = recv - sent;
         if (error)
@@ -27,28 +53,32 @@ async function pingCheck(target)
         }
         else
         {
-          resolve(ms);
+          resolve(ms + ' ' + target);
         }
     })
   })
-  return promise
+  return promise;
 }
 
-//We can use this as our socket setup
+
+
+
 for (ip of IPs)
 {
-  console.log(ip);
   pingCheck(ip)
-    .then((resolution)=>
+    .then(async (resolution)=>
     {
+      console.log(resolution);
     })
-    .catch((error)=>
+    .catch(async (error)=>
     {
-      if(error.name = 'RequestTimeOutError')
+      if(error.name == 'RequestTimeOutError')
       {
-        console.log("time out")
+        console.log(error,ip)
       }
       else
-        console.log("Other error");
+        console.log("Other error", error )
     });
 }
+
+*/
